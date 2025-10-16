186e627fdbc82874ac32a409b8cdf05245e9893d
robertchitic <rc8528i@gre.ac.uk>
2025-10-16 12:49:45 +0100

    Added kt classes, Event, Participant, Venue

--- src/Main/Kotlin/com/eventplanning/domain/Event.kt
+++ src/Main/Kotlin/com/eventplanning/domain/Event.kt
@@ -1,2 +1,20 @@
 package com.eventplanning.domain
 
+import com.eventplanning.domain.Venue
+
+import java.time.LocalDateTime
+
+data class Event(
+    val id: String,
+    val title: String,
+    val dateTime: LocalDateTime,
+    val venue: Venue,
+    val description: String = "",
+    val maxParticipants: Int = venue.capacity,
+    val registrationParticipants: Int = venue.capacity ,
+
+
+
+
+
+)
