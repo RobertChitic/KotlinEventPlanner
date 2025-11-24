0ac6439dd4547256630bec6145d001612c116e70
robertchitic <rc8528i@gre.ac.uk>
2025-11-24 12:41:27 +0000

    Fixed the Main, Removed Scala Mention in Main (not required)

--- src/Main/Kotlin/com/eventplanning/Main.kt
+++ src/Main/Kotlin/com/eventplanning/Main.kt
@@ -5,6 +5,7 @@ import com.eventplanning.persistence.DataStore
 import com.eventplanning.ui.MainWindow
 import javax.swing.SwingUtilities
 
+
 fun main() {
     // Initialize database
     val dataStore = DataStore()
